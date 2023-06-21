77e9ef0aa1ebe45b698957308a8b5962ada0aa61
Javier <javiteen1250@gmail.com>
2023-06-21 18:36:19 -0300

    teoria de spread operator e implementacion con arrays y objetos

--- Modulo2/Clase 11/app.js
+++ Modulo2/Clase 11/app.js
@@ -1,3 +1,4 @@
+console.log("\n ---DESTRUCTURING--- \n");
 //Destructuring con un objeto
 let persona = {
     nombre : "jack",
@@ -12,7 +13,7 @@ console.log(nombre);
 console.log(color);
 
 //Destructuring con una lista
-console.log("----------------------------- \n Destructuring con una lista");
+console.log("------------------------ \n Destructuring con una lista");
 
 let frutas = ["Manzana", "Pera", "Uvas", "Banana","Naranja"];
 
@@ -20,4 +21,33 @@ let [Manzana, Naranja] = frutas;
 
 console.log(Manzana);
 console.log(Naranja);
+
+console.log("\n ---SPREAD OPERATOR--- \n");
+
+let carros = ["Ford", "Chevi", "Mercedes", "Honda","Toyota"];
+let motos = ["husqvarna", "motomel", "Ducati", "BMW"];
+let veichulos = [...carros,...motos]
+console.log(veichulos);
+
+console.log("------------------------ \n Destructuring con un objeto");
+
+let cuentaCorriente = {
+    saldo : 10,
+    deuda : 3000000,
+    alias : "mvc crk"
+}
+
+let pablo = {
+    nombre : "Pablo",
+    dni : 6023493,
+    ...cuentaCorriente
+}
+
+let julian = {
+    nombre : "Julian",
+    dni : 6023492,
+    ...cuentaCorriente
+}
+
+console.log(pablo);
 
