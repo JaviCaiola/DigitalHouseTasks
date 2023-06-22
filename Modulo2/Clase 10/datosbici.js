88372e6154f4a9172dcb03d83e079e0a865d0293
Javier <javiteen1250@gmail.com>
2023-06-22 10:26:14 -0300

    resolucion del ejercicio 1

---
+++ Modulo2/Clase 10/datosbici.js
@@ -0,0 +1,11 @@
+let fs = require('fs');
+
+const importarDatos = () => {
+    let datos = fs.readFileSync(__dirname +"/ejercicio1.js","Utf-8");
+    let bicis = JSON.parse(datos);
+    return bicis;
+}
+
+let datos = importarDatos();
+
+module.exports = importarDatos;
---
+++ Modulo2/Clase 10/ejercicio1.js
@@ -0,0 +1,62 @@
+[
+    {
+      "id": 1,
+      "marca": "Fierce",
+      "modelo": "FM18SI29AM211",
+      "rodado": 16,
+      "anio": 2019,
+      "color": "Celeste",
+      "peso": 13,
+      "tipo": "Montañera",
+      "precio": 20000,
+      "vendida": "si"
+    },
+    {
+      "id": 2,
+      "marca": "Shifter",
+      "modelo": "FM18F29AM210",
+      "rodado": 26,
+      "anio": 2020,
+      "color": "negro",
+      "peso": 15,
+      "tipo": "Paseo",
+      "precio": 600000,
+      "vendida": "no"
+    },
+    {
+      "id": 3,
+      "marca": "Olmo",
+      "modelo": "Regal",
+      "rodado": 28,
+      "anio": 2021,
+      "color": "gris",
+      "peso": 16,
+      "tipo": "Retro",
+      "precio": 40000,
+      "vendida": "si"
+    },
+    {
+      "id": 4,
+      "marca": "Battle",
+      "modelo": "Sunshine",
+      "rodado": 29,
+      "anio": 2022,
+      "color": "amarillo",
+      "peso": 17,
+      "tipo": "Triatlón",
+      "precio": 21000,
+      "vendida": "si"
+    },
+    {
+      "id": 5,
+      "marca": "TopMega",
+      "modelo": "Techno",
+      "rodado": 26,
+      "anio": 2023,
+      "color": "rosa",
+      "peso": 13,
+      "tipo": "comun",
+      "precio": 340000,
+      "vendida": "no"
+    }
+]
