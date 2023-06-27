f0325a4fae360c476d43ccb05311c7c7d672d64d
Javier <javiteen1250@gmail.com>
2023-06-27 19:44:42 -0300

    resolucion del desafio extra

--- Modulo2/Clase 11/extra.js
+++ Modulo2/Clase 11/extra.js
@@ -3,11 +3,15 @@ for (let i =0; i<=9;i++){
     numeros.push(Math.floor(Math.random() * 100));
 }
 
-let [num0, ,num2, ,num4]  = numeros;
+let [num0, ,num2, ,num4,...otros]  = numeros;
 
+let deportados = [numeros[1], numeros[3],...otros];
 
-
-console.log(num0);
+console.log("todos los numeros: "+numeros+"\n");
+console.log("posicion 0: "+num0+"\n");
+console.log("posicion 2: "+num2+"\n");
+console.log("posicion 4: "+num4+"\n");
+console.log("\n numeros que fueron exiliados:\n"+deportados);
 
 //PARTE DOS DEL DESAFIO EXTRA
 let mascota = {
